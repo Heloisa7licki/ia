56c9a0e65e07962fab60f9213d4bd2d8426f0c16
Heloisa 7licki <145134125+Heloisa7licki@users.noreply.github.com>
2025-10-09 11:47:01 -0300

    j

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
 const perguntas = [
     {
         enunciado: "Você utiliza com frequência as ferramentas básicas em IA?",
@@ -34,23 +33,19 @@ const perguntas = [
         alternativas: ["Sim", "Não"]
     },
 ];
-
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
-
 function mostraPergunta() {
     if (atual >= perguntas.legth) {
         mostraResultado();
         return;
     }
-
-    perguntaAtual = perguntas[atual];
+ perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
@@ -58,7 +53,6 @@ function mostraAlternativas() {
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal = afirmacoes + " ";
